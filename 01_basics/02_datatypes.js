f46af90f19766ece940e2fcbbdaff4b4086122da
eklavya2001 <92029554+eklavya2001@users.noreply.github.com>
2023-12-18 19:34:33 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all js code as newer version
+
+//alert("hello")//we are using nodejs,not browser
+
+console.log(3+3); console.log("eklavya")//code readability should be high
+
+let name = "eklavyad"
+let age = 18
+let isLoggedIn = false
+// primitive data type ha ye sab
+//number =>2 to power 53 is the range(approx)
+//bigint
+//string=>"" 
+//boolean=> true/false
+//null=>standalone value(empty value)intentionally bata raha hu ki khali ha
+//undefined =>
+//symbol=>to find uniqueness(figma tools)/unique
+
+
+//object
+
+console.log(typeof null)//null is an object(interview question h kai bar puche jate ha)
+console.log(typeof undefined)//undefined is undefined
+
