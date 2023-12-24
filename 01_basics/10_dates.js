06e9382265d3b041537e04a8dcf0e0e46401008b
eklavya2001 <92029554+eklavya2001@users.noreply.github.com>
2023-12-24 12:26:17 +0000

    date and time in js

---
+++ 01_basics/10_dates.js
@@ -0,0 +1,28 @@
+let myDate = new Date()
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+
+console.log(myDate.toLocaleString('en-IN'))
+console.log(typeof myDate)
+
+let myCreatedDate = new Date(2023, 0, 23)
+console.log(myCreatedDate.toDateString())
+let date= new Date("01-14-2023")
+console.log(date.toLocaleDateString())
+
+
+let myTimeStamp = Date.now()// yaha sab se jo time aata ha wo ms me aata ha so s me lane ka liye divide by 1000 and usko round off kardo line no 20
+console.log(myTimeStamp)
+console.log(date.getTime())
+
+
+console.log(Math.floor(Date.now()/1000))
+
+
+let nayaDate = new Date()
+console.log(nayaDate.getMonth())
+
+
+
+console.log(nayaDate.toLocaleString('default',{weekday:"long"}))//customize karke date and time likhne ka tarika, we shall remember this.
